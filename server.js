d1eb3cc1059b56ebab26a30c43b5b5b4fccd976f
justingreco <justin.greco@raleighnc.gov>
2016-09-22 13:07:49 -0400

    fixed issue with crossdomain

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ var router = express.Router();              // get an instance of the express Ro
 router.use(function(req, res, next) {
   // do logging
   console.log('Something is happening.');
+  res.header("Access-Control-Allow-Origin", "*");  
   res.header("Access-Control-Allow-Headers",  "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
   res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS,DELETE");
   next(); // make sure we go to the next routes and don't stop here
